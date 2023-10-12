d2c76c7c5f6343819dc6335b64b4dcd2e56651c1
Joao Jesus <HCC-JJesus@MBPLC.com>
2023-10-12 14:26:23 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Container, Dropdown, Sticky, Menu } from 'semantic-ui-react'
+import {  useState } from 'react'
+
+const languageOptions = [
+  { key: 'Arabic', text: 'Arabic', value: 'Arabic' },
+  { key: 'Chinese', text: 'Chinese', value: 'Chinese' },
+  { key: 'Danish', text: 'Danish', value: 'Danish' },
+  { key: 'Dutch', text: 'Dutch', value: 'Dutch' },
+  { key: 'English', text: 'English', value: 'English' },
+  { key: 'French', text: 'French', value: 'French' },
+  { key: 'German', text: 'German', value: 'German' },
+  { key: 'Greek', text: 'Greek', value: 'Greek' },
+  { key: 'Hungarian', text: 'Hungarian', value: 'Hungarian' },
+  { key: 'Italian', text: 'Italian', value: 'Italian' },
+  { key: 'Japanese', text: 'Japanese', value: 'Japanese' },
+  { key: 'Korean', text: 'Korean', value: 'Korean' },
+  { key: 'Lithuanian', text: 'Lithuanian', value: 'Lithuanian' },
+  { key: 'Persian', text: 'Persian', value: 'Persian' },
+  { key: 'Polish', text: 'Polish', value: 'Polish' },
+  { key: 'Portuguese', text: 'Portuguese', value: 'Portuguese' },
+  { key: 'Russian', text: 'Russian', value: 'Russian' },
+  { key: 'Spanish', text: 'Spanish', value: 'Spanish' },
+  { key: 'Swedish', text: 'Swedish', value: 'Swedish' },
+  { key: 'Turkish', text: 'Turkish', value: 'Turkish' },
+  { key: 'Vietnamese', text: 'Vietnamese', value: 'Vietnamese' },
+]
 
 function App() {
+
+  const [language, setLanguage] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Sticky>
+          <Menu
+            attached='top'
+            tabular
+            style={{ backgroundColor: '#fff', paddingTop: '1em' }}
+          >
+            
+            <Menu.Menu position='right'>
+              <Menu.Item>
+              <Dropdown
+                  button
+                  className='icon'
+                  floating
+                  labeled
+                  icon='world'
+                  options={languageOptions}
+                  search
+                  text='Select Language'
+                  value={language}
+                  onChange={(e, data : any) => setLanguage(data.value ?? "")}
+                />
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu>
+        </Sticky>        
+      <Container className='container-padding'>
+        <SearchComponent></SearchComponent>    
+      </Container>  
+    </>
   );
 }
 
